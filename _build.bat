2d00fe213a21c321142f87ab2922dd523177b896
readermode <101894133+readermode@users.noreply.github.com>
2022-03-18 23:12:25 +0000

    Update _build.bat

--- _build.bat
+++ _build.bat
@@ -1,3 +1,7 @@
 Powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco upgrade chocolatey
+choco upgrade git
+choco upgrade visualstudio2022buildtools
+
 echo done
